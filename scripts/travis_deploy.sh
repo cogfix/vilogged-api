9c41c4ca826a1fc8e3c4669b45bf1c903c15c8e2
Musa Musa <musa@musamusa.com>
2016-04-08 07:27:23 +0100

    added server config files

---
+++ scripts/travis_deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+set -x
+
+TAG=$TRAVIS_TAG
+BRANCH=$TRAVIS_BRANCH
+PR=$TRAVIS_PULL_REQUEST
+
+echo $TAG
+echo $BRANCH
+echo $PR
+
+if [ -z $TAG ]
+then
+    echo "No tags, tagging as: latest"
+    TAG="latest"
+fi
+
+# if this is on the develop branch and this is not a PR, deploy it
+if [ $BRANCH = "master" -a $PR = "false" ]
+then
+    docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+    docker tag -f $DOCKER_USERNAME/vilogged-server:$TAG
+    docker push $DOCKER_USERNAME/vilogged-server:$TAG
+
+fi
